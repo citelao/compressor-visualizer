1efaccad6358083048316675b4b1d727137fbfbc
Ben Stolovitz <ben@stolovitz.com>
2020-11-23 16:55:13 -0500

    clarify other stuff

--- src/js/Compressor.ts
+++ src/js/Compressor.ts
@@ -15,15 +15,16 @@ export default class Compressor {
 
     // https://www.w3.org/TR/webaudio/#compression-curve
     public static compressLinear(linearValue: number, compressor: ICompressorSettings): number {
-        // TODO
         const linearThreshold = Db.dbToLinear(compressor.threshold);
         const linearKneeEnd = Db.dbToLinear(compressor.threshold + compressor.knee);
-        console.log(`threshold: ${linearThreshold}; knee end: ${linearKneeEnd}`)
+        // console.log(`threshold: ${linearThreshold}; knee end: ${linearKneeEnd}`);
         if (linearValue < linearThreshold) {
             return linearValue;
         } else if (linearValue < linearKneeEnd) {
             // User-agent dependent
-            // TODO
+            //
+            // TODO: our knee settings default to 0 to make this a non-issue for
+            // now :)
             return linearValue;
         } else {
             // Note: this diverges from the spec for the compression curve
@@ -33,6 +34,9 @@ export default class Compressor {
             // (https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/renderer/platform/audio/dynamics_compressor_kernel.cc;l=117;bpv=1;bpt=1),
             // so follow the Chromium model and apply the ration "after" the
             // threshold limit.
+            //
+            // TODO: should this knee be done on the linear value or the the dB
+            // value?
             return linearKneeEnd + ((1 / compressor.ratio) * (linearValue - linearKneeEnd));
         }
     }
